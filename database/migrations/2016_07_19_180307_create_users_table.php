6f498d21bfc72214f4ec848a158d05fbf144e441
volodymyr <maslechkin.v@gmail.com>
2016-07-16 21:43:55 +0300

    M-1 Create models, controllers, tables

--- database/migrations/2016_07_19_180307_create_users_table.php
+++ database/migrations/2016_07_19_180307_create_users_table.php
@@ -13,11 +13,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDb';
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->integer('id_role');
+            $table->string('name')->default(null);
+            $table->string('lastname')->default(null);
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('active')->default(0);
+            $table->tinyInteger('deleted')->default(0);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2016_07_25_275837_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->engine = 'InnoDb';
+            $table->increments('id');
+            $table->integer('id_user_to');
+            $table->integer('id_sender');
+            $table->text('message');
+            $table->tinyInteger('is_new')->default(1);
+            $table->tinyInteger('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+}
